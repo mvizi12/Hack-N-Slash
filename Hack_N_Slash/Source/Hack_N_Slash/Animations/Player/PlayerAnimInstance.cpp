5ff66fe6e2abd220cf73d613713783550ac3f4e0
Miles Vizinau <mvizi12@gmail.com>
2024-12-30 21:36:21 -0800

    Motion Matching
    -Enabled "Chooser" plugin
    -Retargeted animations

--- Hack_N_Slash/Source/Hack_N_Slash/Animations/Player/PlayerAnimInstance.cpp
+++ Hack_N_Slash/Source/Hack_N_Slash/Animations/Player/PlayerAnimInstance.cpp
@@ -3,3 +3,35 @@
 
 #include "PlayerAnimInstance.h"
 
+/*PRIVATE FUNCTIONS*/
+void UPlayerAnimInstance::UpdateVelocity()
+{
+    if (!IsValid(playerPawn)) {return;}
+
+    currentVeloctiy = playerPawn->GetVelocity();
+}
+
+
+/*PROTECTED FUNCTIONS*/
+void UPlayerAnimInstance::UpdatePlayerPawn()
+{
+    playerPawn = TryGetPawnOwner();
+}
+
+void UPlayerAnimInstance::UpdateCurrentAngle()
+{
+    if (!IsValid(playerPawn)) {return;}
+
+    UpdateVelocity();
+    FRotator currentRotation = playerPawn->GetActorRotation();
+    currentAngle = CalculateDirection(currentVeloctiy, currentRotation);
+    //UKismetAnimationLibrary::CalculateDirection(currentVeloctiy, currentRotation);
+}
+
+void UPlayerAnimInstance::UpdateCurrentSpeed()
+{
+    if (!IsValid(playerPawn)) {return;}
+
+    UpdateVelocity();
+    currentSpeed = static_cast<float>(currentVeloctiy.Length());
+}
--- Hack_N_Slash/Source/Hack_N_Slash/Animations/Player/PlayerAnimInstance.h
+++ Hack_N_Slash/Source/Hack_N_Slash/Animations/Player/PlayerAnimInstance.h
@@ -13,5 +13,30 @@ UCLASS()
 class HACK_N_SLASH_API UPlayerAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+
+	//Unreal doesn't suggest private variables be exposed to blueprint
+	private:
+		APawn* playerPawn;
+
+		void UpdateVelocity();
+
+	protected:
+		UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		FVector currentVeloctiy{0.0f, 0.0f, 0.0f};
+
+		UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		float currentSpeed {0.0f};
+
+		UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		float currentAngle {0.0f};
+
+		UFUNCTION(BlueprintCallable)
+		void UpdatePlayerPawn();
+
+		UFUNCTION(BlueprintCallable)
+		void UpdateCurrentAngle();
+
+		UFUNCTION(BlueprintCallable)
+		void UpdateCurrentSpeed();
 	
 };
