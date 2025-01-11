aec31a85f32f3d78cc96154b67f7460f92899d8c
Miles Vizinau <mvizi12@gmail.com>
2025-01-10 20:44:47 -0800

    Added Player Dodge Functionality

--- Hack_N_Slash/Source/Hack_N_Slash/Characters/Player/PlayerActionsComponent.cpp
+++ Hack_N_Slash/Source/Hack_N_Slash/Characters/Player/PlayerActionsComponent.cpp
@@ -3,6 +3,8 @@
 
 #include "PlayerActionsComponent.h"
 #include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Engine/Engine.h"
 #include "C:\Users\mvizi\Documents\Unreal Projects\Hack-N-Slash\Hack_N_Slash\Source\Hack_N_Slash\Interfaces\MainPlayerI.h"
 #include "C:\Users\mvizi\Documents\Unreal Projects\Hack-N-Slash\Hack_N_Slash\Source\Hack_N_Slash\Interfaces\Fighter.h"
 
@@ -14,13 +16,13 @@ UPlayerActionsComponent::UPlayerActionsComponent()
 	PrimaryComponentTick.bCanEverTick = true;
 }
 
-
 // Called when the game starts
 void UPlayerActionsComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
 	characterRef = GetOwner<ACharacter>();
+	movementComp = characterRef->GetCharacterMovement();
 	iPlayerRef = Cast<IMainPlayerI>(characterRef);
 	iFighterRef = Cast<IFighter>(characterRef);
 }
@@ -31,9 +33,42 @@ void UPlayerActionsComponent::TickComponent(float DeltaTime, ELevelTick TickType
 }
 
 /************************************Private Functions************************************/
+bool UPlayerActionsComponent::CanDodge()
+{
+    TArray<EState> invalidDodgeStates = {EState::Attack, EState::Death, EState::Dodge}; //States the player isn't allowed to attack
+	return !iFighterRef->IsCurrentStateEqualToAny(invalidDodgeStates) && !movementComp->IsFalling();
+}
+
+void UPlayerActionsComponent::PerformDodge()
+{
+	if (GEngine && bDebugMode) {GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Red, TEXT("Performing Dodge"));}
+	characterRef->PlayAnimMontage(dodgeMontage);
+	OnDodgeDelegate.Broadcast(staminaCostDodge);
+}
 /************************************Private Functions************************************/
 
 /************************************Protected Functions************************************/
+void UPlayerActionsComponent::Dodge()
+{
+	if (dodgeMontage == nullptr) {return;}
+	if (iFighterRef == nullptr || iPlayerRef == nullptr) {return;}
+	if (!iPlayerRef->HasEnoughStamina(staminaCostDodge)) {return;}
+
+	//TArray<EState> states = {EState::Attack, EState::Dodge}; //Can cancel attacks and dodges
+	TArray<EState> states = {EState::Attack};
+	if (iFighterRef->IsCurrentStateEqualToAny(states))
+	{
+		bSavedDodge = true;
+		if (GEngine && bDebugMode) {GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Red, TEXT("Saved Dodge"));}
+	}
+	else
+	{
+		//Else Attempt dodge
+		if (GEngine && bDebugMode) {GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Red, TEXT("Attempting Light Attack"));}
+		if (CanDodge()) {PerformDodge();}
+	}
+}
+
 void UPlayerActionsComponent::Jump()
 {
 	if (iPlayerRef == nullptr || iFighterRef == nullptr) {return;}
--- Hack_N_Slash/Source/Hack_N_Slash/Characters/Player/PlayerActionsComponent.h
+++ Hack_N_Slash/Source/Hack_N_Slash/Characters/Player/PlayerActionsComponent.h
@@ -6,6 +6,7 @@
 #include "Components/ActorComponent.h"
 #include "PlayerActionsComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_SPARSE_DELEGATE_OneParam(FOnDodgeSignature, UPlayerActionsComponent, OnDodgeDelegate, float, cost);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class HACK_N_SLASH_API UPlayerActionsComponent : public UActorComponent
@@ -14,16 +15,37 @@ class HACK_N_SLASH_API UPlayerActionsComponent : public UActorComponent
 
 private:
 	ACharacter* characterRef;
+	class UCharacterMovementComponent* movementComp;
 	class IMainPlayerI* iPlayerRef;
 	class IFighter* iFighterRef;
 
+	bool bSavedDodge {false};
+
+	bool CanDodge();
+	void PerformDodge();
+
 protected:
+	UPROPERTY(EditAnywhere)
+	bool bDebugMode;
+	
+	UPROPERTY(EditDefaultsOnly)
+	UAnimMontage* dodgeMontage;
+
+	UPROPERTY(EditAnywhere)
+	float staminaCostDodge {5.0f};
+
 	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable)
+	void Dodge();
 
 	UFUNCTION(BlueprintCallable)
 	void Jump();
 
-public:	
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnDodgeSignature OnDodgeDelegate;
+
 	UPlayerActionsComponent();
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 };
